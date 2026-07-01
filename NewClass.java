de83bec5dd3e43508b97db36b782344be51d6ea2
anubhav <anubhavsaxenavibhu@gmail.com>
2026-07-01 14:12:41 +0530

    this program is used to implement for each loop

---
+++ NewClass.java
@@ -0,0 +1,9 @@
+// this program si to implement for each loop
+public class NewClass {
+    public static void main(String[] args){
+        int j[] = {10,20,30};
+        for( int m: j){
+            System.out.println(m);
+        }
+    }
+}
