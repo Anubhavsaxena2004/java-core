a5dc21b06bc956db6802172a940ccf96fae00f8c
anubhav <anubhavsaxenavibhu@gmail.com>
2026-07-02 14:58:10 +0530

    done one more exception on strings

---
+++ exception4.java
@@ -0,0 +1,10 @@
+class exception4{
+public static void main(String[] arg){
+try{
+String sc = null ;
+System.out.println(sc.toUpperCase());
+}
+catch(Exception k){
+System.out.println("string is null");
+}
+}}
