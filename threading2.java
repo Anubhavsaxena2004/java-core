670cdec678ba4b410c3e38ff510e56272f59fda1
anubhav <anubhavsaxenavibhu@gmail.com>
2026-07-06 16:20:50 +0530

    this commit shows how to implement basic thread program

---
+++ threading2.java
@@ -0,0 +1,20 @@
+class threading2 implements Runnable{
+public void run(){
+    for(int i = 0; i <= 10; i++){
+        try {
+            System.out.println(i);
+            Thread.sleep(2000);
+        }
+        catch(Exception e) {
+            System.out.println("entered correct: " + e);
+        }
+    }
+}  
+
+public static void main(String args[]){
+    threading2 p = new threading2();
+    Thread m = new Thread(p);
+    m.start();
+
+}
+}
