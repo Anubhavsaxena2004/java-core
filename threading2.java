31fc6cfe1f7eca8313d1b092de4a6ed52f92002f
anubhav <anubhavsaxenavibhu@gmail.com>
2026-07-07 13:18:57 +0530

    this commit shows how to implement basic thread program for 50 times loop

--- threading2.java
+++ threading2.java
@@ -1,6 +1,6 @@
 class threading2 implements Runnable{
 public void run(){
-    for(int i = 0; i <= 10; i++){
+    for(int i = 0; i <= 50; i++){
         try {
             System.out.println(i);
             Thread.sleep(2000);
